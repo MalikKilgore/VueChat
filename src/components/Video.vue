90ed14e709c6215ab038237d8558744760697619
Malik Kilgore <malik199228@gmail.com>
2020-12-06 17:08:48 -0500

    Redid the video call implementation to use Firebase Cloudstore

--- src/components/Video.vue
+++ src/components/Video.vue
@@ -1,225 +1,342 @@
 <template>
   <div class="videoRoot">
-    <h1>This section is in development.</h1>
+    <div>
+      <span id="currentRoom"></span>
+    </div>
     <div class="videoCall">
-      <video id="localVideo" width="450" height="300" playsinline autoplay muted></video>
-      <video id="remoteVideo" width="450" height="300" playsinline autoplay></video>
+      <video
+        id="localVideo"
+        width="450"
+        height="300"
+        playsinline
+        autoplay
+        muted
+      ></video>
+      <video
+        id="remoteVideo"
+        width="450"
+        height="300"
+        playsinline
+        autoplay
+      ></video>
     </div>
     <br />
     <div class="box">
-      <button id="startButton" v-on:click="start">Start</button>
-      <button id="callButton" v-on:click="call">Call</button>
-      <button id="hangupButton" v-on:click="hangup">Hang Up</button>
+      <button id="cameraBtn" v-on:click="openUserMedia">Start</button>
+      <button id="createBtn" v-on:click="createRoom">Call</button>
+      <button id="hangupBtn" v-on:click="hangup">Hang Up</button>
     </div>
     <br />
-    <div class="box">
-      <span>SDP Semantics:</span>
-      <select id="sdpSemantics">
-        <option selected value="">Default</option>
-        <option value="unified-plan">Unified Plan</option>
-        <option value="plan-b">Plan B</option>
-      </select>
+    <div id="room-dialog">
+      <h2>Enter ID for room to join:</h2>
+      <div class="text-field">
+        <input type="text" id="room-id" placeholder="Enter room ID here...">
+        <button id="joinBtn" v-on:click="joinRoom">Join Room</button>
+      </div>
     </div>
   </div>
 </template>
 <script>
-import Vue from 'vue'
-import Vuex from 'vuex'
-import router from '../router'
-import store from '../store'
+import Vue from "vue";
+import Vuex from "vuex";
+import router from "../router";
+import store from "../store";
+import firebase from "firebase/app";
+import {
+  db,
+  usersCollection,
+  programChat,
+  networkChat,
+  creativeChat,
+  generalChat,
+  bugChat,
+  videoRooms,
+} from "../firebase/firebase.js";
 
 export default {
-    name: 'Video',
+  name: "Video",
   data() {
     return {
-        startTime: null,
-        localVideo: {
-          srcObject: null,
-        },
-        remoteVideo: {
-          srcObject: null,
-        },
-        localStream: null,
-        remoteStream: null,
-        pc1: null,
-        pc2: null,
-        offerOptions: {
-            offerToReceiveAudio: 1,
-            offerToReceiveVideo: 1  
-        },
-        buttons: {
-            startButton: {
-                disabled: false
-            },
-            callButton: {
-                disabled: true
-            },
-            hangupButton: {
-                disabled: true
-            },        
-        },
+      localVideo: null,
+      remoteVideo: null,
 
-    }
+      peerConnection: null,
+      localStream: null,
+      remoteStream: null,
+      /*roomDialog: new mdc.dialog.MDCDialog(
+        document.querySelector("#room-dialog")
+      ),*/
+      roomId: null,
+      configuration: {
+        iceServers: [
+          {
+            urls: [
+              "stun:stun1.l.google.com:19302", 
+              "stun:stun2.l.google.com:19302",
+              "stun:stun.services.mozilla.com"
+            ]
+          }
+        ]
+      },
+        iceCandidatePoolSize: 2,
+    };
   },
   methods: {
-    async start() {
-        console.log('Requesting local stream');
-        this.buttons.startButton.disabled = true;
-        try {
-            const stream = await navigator.mediaDevices.getUserMedia({audio: true, video: true});
-            console.log('Received local stream');
-            document.getElementById('localVideo').srcObject = stream
-            this.localVideo.srcObject = stream;
-            this.localStream = stream;
-            this.buttons.callButton.disabled = false;
-        } catch (e) {
-            alert(`getUserMedia() error: ${e.name}`);
+    async createRoom() {
+      document.querySelector("#createBtn").disabled = true;
+      document.querySelector("#joinBtn").disabled = true;
+      const roomRef = await videoRooms.doc();
+
+      console.log(
+        "Create PeerConnection with configuration: ",
+        this.configuration
+      );
+      this.peerConnection = new RTCPeerConnection(this.configuration);
+
+      this.registerPeerConnectionListeners();
+
+      this.localStream.getTracks().forEach((track) => {
+        this.peerConnection.addTrack(track, this.localStream);
+      });
+
+      // Code for collecting ICE candidates below
+      const callerCandidatesCollection = roomRef.collection("callerCandidates");
+      this.peerConnection.addEventListener("icecandidate", (event) => {
+        if (!event.candidate) {
+          console.log("Got final candidate!");
+          return;
         }
-    },
-    async call() {
-        this.buttons.callButton.disabled = true;
-        this.buttons.hangupButton.disabled = false;
-        console.log('Starting call');
-        this.startTime = window.performance.now();
-        const videoTracks = this.localStream.getVideoTracks();
-        const audioTracks = this.localStream.getAudioTracks();
-        if (videoTracks.length > 0) {
-            console.log(`Using video device: ${videoTracks[0].label}`);
+        console.log("Got candidate: ", event.candidate);
+        callerCandidatesCollection.add(event.candidate.toJSON());
+      });
+
+      // Code for creating a room below
+      const offer = await this.peerConnection.createOffer();
+      await this.peerConnection.setLocalDescription(offer);
+      console.log("Created offer:", offer);
+
+      const roomWithOffer = {
+        offer: {
+          type: offer.type,
+          sdp: offer.sdp,
+        },
+      };
+
+      await roomRef.set(roomWithOffer);
+      this.roomId = roomRef.id;
+      console.log(`New room created with SDP offer. Room ID: ${roomRef.id}`);
+      document.querySelector(
+        "#currentRoom"
+      ).innerText = `Current room is ${roomRef.id} - You are the caller!`;
+      // Code for creating a room above
+
+      this.peerConnection.addEventListener("track", (event) => {
+        console.log("Got remote track:", event.streams[0]);
+        event.streams[0].getTracks().forEach((track) => {
+          console.log("Add a track to the remoteStream:", track);
+          this.remoteStream.addTrack(track);
+        });
+      });
+
+      // Listening for remote session description below
+      roomRef.onSnapshot(async (snapshot) => {
+        const data = snapshot.data();
+        if (!this.peerConnection || !this.peerConnection.remoteDescription) {
+          console.log("Got remote description: ", data.answer);
+          const rtcSessionDescription = new RTCSessionDescription(data.answer);
+          await this.peerConnection.setRemoteDescription(rtcSessionDescription);
         }
-        if (audioTracks.length > 0) {
-            console.log(`Using audio device: ${audioTracks[0].label}`);
-        }
-        const configuration = this.getSelectedSdpSemantics();
-        console.log('RTCPeerConnection configuration:', configuration);
-        this.pc1 = new RTCPeerConnection(configuration);
-        console.log('Created local peer connection object pc1');
-        this.pc1.addEventListener('icecandidate', e => this.onIceCandidate(this.pc1, e));
-        this.pc2 = new RTCPeerConnection(configuration);
-        console.log('Created remote peer connection object pc2');
-        this.pc2.addEventListener('icecandidate', e => this.onIceCandidate(this.pc2, e));
-        this.pc1.addEventListener('iceconnectionstatechange', e => this.onIceStateChange(this.pc1, e));
-        this.pc2.addEventListener('iceconnectionstatechange', e => this.onIceStateChange(this.pc2, e));
-        this.pc2.addEventListener('track', this.gotRemoteStream);
+      });
 
-        this.localStream.getTracks().forEach(track => this.pc1.addTrack(track, this.localStream));
-        console.log('Added local stream to pc1');
+      // Listen for remote ICE candidates below
+      roomRef.collection("callerCandidates").onSnapshot((snapshot) => {
+        snapshot.docChanges().forEach(async (change) => {
+          if (change.type === "added") {
+            let data = change.doc.data();
+            console.log(
+              `Got new remote ICE candidate: ${JSON.stringify(data)}`
+            );
+            //THIS IS THROWING ERROR
+            await this.peerConnection.addIceCandidate(
+              new RTCIceCandidate(data)
+            );
+          }
+        });
+      });
+      // Listen for remote ICE candidates above
+    },
+    async joinRoom() {
+      document.querySelector("#createBtn").disabled = true;
+      document.querySelector("#joinBtn").disabled = true;
 
-        try {
-            console.log('pc1 createOffer start');
-            const offer = await this.pc1.createOffer(this.offerOptions);
-            await this.onCreateOfferSuccess(offer);
-        } catch (e) {
-            this.onCreateSessionDescriptionError(e);
-        }
+          this.roomId = document.querySelector("#room-id").value;
+          console.log("Join room: ", this.roomId);
+          document.querySelector(
+            "#currentRoom"
+          ).innerText = `Current room is ${this.roomId} - You are the callee!`;
+          await joinRoomById(this.roomId);
+        
+        { once: true }
+      
+      //roomDialog.open();
     },
-    hangup() {
-        console.log('Ending call');
-        this.pc1.close();
-        this.pc2.close();
-        this.pc1 = null;
-        this.pc2 = null;
-        this.buttons.hangupButton.disabled = true;
-        this.buttons.callButton.disabled = false;
-    },
-    getName(pc) {
-      return (pc === this.pc1) ? 'pc1' : 'pc2';
-    },
-    getOtherPc(pc) {
-      return (pc === this.pc1) ? this.pc2 : this.pc1;
-    },
-    getSelectedSdpSemantics() {
-      const sdpSemanticsSelect = document.querySelector('#sdpSemantics');
-      const option = sdpSemanticsSelect.options[sdpSemanticsSelect.selectedIndex];
-      return option.value === '' ? {} : {sdpSemantics: option.value};
-    },
-    async onCreateOfferSuccess(desc) {
-      console.log(`Offer from pc1\n${desc.sdp}`);
-      console.log('pc1 setLocalDescription start');
-      try {
-        await this.pc1.setLocalDescription(desc);
-        this.onSetLocalSuccess(this.pc1);
-      } catch (e) {
-        this.onSetSessionDescriptionError();
-      }
+    async joinRoomById(roomId) {
+      const roomRef = videoRooms.doc(`${this.roomId}`);
+      const roomSnapshot = await roomRef.get();
+      console.log("Got room:", roomSnapshot.exists);
 
-      console.log('pc2 setRemoteDescription start');
-      try {
-        await this.pc2.setRemoteDescription(desc);
-        this.onSetRemoteSuccess(this.pc2);
-      } catch (e) {
-        this.onSetSessionDescriptionError();
-      }
+      if (roomSnapshot.exists) {
+        console.log(
+          "Create PeerConnection with configuration: ",
+          configuration
+        );
+        this.peerConnection = new RTCPeerConnection(configuration);
+        this.registerPeerConnectionListeners();
+        this.localStream.getTracks().forEach((track) => {
+          this.peerConnection.addTrack(track, localStream);
+        });
 
-      console.log('pc2 createAnswer start');
-      // Since the 'remote' side has no media stream we need
-      // to pass in the right constraints in order for it to
-      // accept the incoming offer of audio and video.
-      try {
-        const answer = await this.pc2.createAnswer();
-        await this.onCreateAnswerSuccess(answer);
-      } catch (e) {
-        this.onCreateSessionDescriptionError(e);
+        // Code for collecting ICE candidates below
+        const callerCandidatesCollection = roomRef.collection(
+          "callerCandidates"
+        );
+        this.peerConnection.addEventListener("icecandidate", (event) => {
+          if (!event.candidate) {
+            console.log("Got final candidate!");
+            return;
+          }
+          console.log("Got candidate: ", event.candidate);
+          callerCandidatesCollection.add(event.candidate.toJSON());
+        });
+        // Code for collecting ICE candidates above
+        this.peerConnection.addEventListener("track", (event) => {
+          console.log("Got remote track:", event.streams[0]);
+          event.streams[0].getTracks().forEach((track) => {
+            console.log("Add a track to the remoteStream:", track);
+            this.remoteStream.addTrack(track);
+          });
+        });
+
+        // Code for creating SDP answer below
+        const offer = roomSnapshot.data().offer;
+        console.log("Got offer:", offer);
+        await this.peerConnection.setRemoteDescription(
+          new RTCSessionDescription(offer)
+        );
+        const answer = await this.peerConnection.createAnswer();
+        console.log("Created answer:", answer);
+        await this.peerConnection.setLocalDescription(answer);
+
+        const roomWithAnswer = {
+          answer: {
+            type: answer.type,
+            sdp: answer.sdp,
+          },
+        };
+        await roomRef.update(roomWithAnswer);
+        // Code for creating SDP answer above
+        // Listening for remote ICE candidates below
+        roomRef.collection("callerCandidates").onSnapshot((snapshot) => {
+          snapshot.docChanges().forEach(async (change) => {
+            if (change.type === "added") {
+              let data = change.doc.data();
+              console.log(
+                `Got new remote ICE candidate: ${JSON.stringify(data)}`
+              );
+              await this.peerConnection.addIceCandidate(
+                new RTCIceCandidate(data)
+              );
+            }
+          });
+        });
+        // Listening for remote ICE candidates above
       }
-    },
-    gotRemoteStream(e) {
-      if (this.remoteVideo.srcObject !== e.streams[0]) {
-        document.getElementById('remoteVideo').srcObject = e.streams[0]
-        this.remoteVideo.srcObject = e.streams[0];
-        console.log('pc2 received remote stream');
-      }
-    },
-    async onCreateAnswerSuccess(desc) {
-      console.log(`Answer from pc2:\n${desc.sdp}`);
-      console.log('pc2 setLocalDescription start');
-      try {
-        await this.pc2.setLocalDescription(desc);
-        this.onSetLocalSuccess(this.pc2);
-      } catch (e) {
-        this.onSetSessionDescriptionError(e);
-      }
-      console.log('pc1 setRemoteDescription start');
-      try {
-        await this.pc1.setRemoteDescription(desc);
-        this.onSetRemoteSuccess(this.pc1);
-      } catch (e) {
-        this.onSetSessionDescriptionError(e);
-      }
-    },
-    async onIceCandidate(pc, event) {
-      try {
-        await (this.getOtherPc(pc).addIceCandidate(event.candidate));
-        this.onAddIceCandidateSuccess(pc);
-      } catch (e) {
-        this.onAddIceCandidateError(pc, e);
-      }
-      console.log(`${this.getName(pc)} ICE candidate:\n${event.candidate ? event.candidate.candidate : '(null)'}`);
     },
 
-    //Error Handlers
-    onIceStateChange(pc, event) {
-      if (pc) {
-        console.log(`${this.getName(pc)} ICE state: ${pc.iceConnectionState}`);
-        console.log('ICE state change event: ', event);
-      }
-    },
-    onAddIceCandidateError(pc, error) {
-      console.log(`${this.getName(pc)} failed to add ICE Candidate: ${error.toString()}`);
-    },
-    onCreateSessionDescriptionError(error) {
-      console.log(`Failed to create session description: ${error.toString()}`);
-    },
-    onSetSessionDescriptionError(error) {
-      console.log(`Failed to set session description: ${error.toString()}`);
-    },
-    onSetRemoteSuccess(pc) {
-      console.log(`${this.getName(pc)} setRemoteDescription complete`);
-    },
-    onSetLocalSuccess(pc) {
-      console.log(`${this.getName(pc)} setLocalDescription complete`);
+    async openUserMedia(e) {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: true,
+      });
+      document.querySelector("#localVideo").srcObject = stream;
+      this.localStream = stream;
+      this.remoteStream = new MediaStream();
+      document.querySelector("#remoteVideo").srcObject = this.remoteStream;
+
+      console.log("Stream:", document.querySelector("#localVideo").srcObject);
+      document.querySelector("#cameraBtn").disabled = true;
+      document.querySelector("#joinBtn").disabled = false;
+      document.querySelector("#createBtn").disabled = false;
+      document.querySelector("#hangupBtn").disabled = false;
     },
 
+    async hangUp(e) {
+      const tracks = document
+        .querySelector("#localVideo")
+        .srcObject.getTracks();
+      tracks.forEach((track) => {
+        track.stop();
+      });
+
+      if (remoteStream) {
+        remoteStream.getTracks().forEach((track) => track.stop());
+      }
+
+      if (this.peerConnection) {
+        this.peerConnection.close();
+      }
+      document.querySelector("#localVideo").srcObject = null;
+      document.querySelector("#remoteVideo").srcObject = null;
+      document.querySelector("#cameraBtn").disabled = false;
+      document.querySelector("#joinBtn").disabled = true;
+      document.querySelector("#createBtn").disabled = true;
+      document.querySelector("#hangupBtn").disabled = true;
+      document.querySelector("#currentRoom").innerText = "";
+
+      // Delete room on hangup
+      if (this.roomId) {
+        const roomRef = videoRooms.doc(this.roomId);
+        const callerCandidates = await roomRef
+          .collection("callerCandidates")
+          .get();
+        callerCandidates.forEach(async (candidate) => {
+          await candidate.ref.delete();
+        });
+        callerCandidates = await roomRef
+          .collection("callerCandidates")
+          .get();
+        callerCandidates.forEach(async (candidate) => {
+          await candidate.ref.delete();
+        });
+        await roomRef.delete();
+      }
+
+      document.location.reload(true);
+    },
+
+    registerPeerConnectionListeners() {
+      this.peerConnection.addEventListener("icegatheringstatechange", () => {
+        console.log(
+          `ICE gathering state changed: ${this.peerConnection.iceGatheringState}`
+        );
+      });
+      this.peerConnection.addEventListener("connectionstatechange", () => {
+        console.log(
+          `Connection state change: ${this.peerConnection.connectionState}`
+        );
+      });
+      this.peerConnection.addEventListener("signalingstatechange", () => {
+        console.log(`Signaling state change: ${this.peerConnection.signalingState}`);
+      });
+
+      this.peerConnection.addEventListener("iceconnectionstatechange ", () => {
+        console.log(
+          `ICE connection state change: ${this.peerConnection.iceConnectionState}`
+        );
+      });
+    },
   },
-}
+};
 </script>
 <style scoped lang="scss">
 .videoRoot {
